11bf56d09e0adb7da926393118f0c062e3d269cf
Jonathan MAhut <jonathan.mahut@gmail.com>
2020-05-18 18:18:32 +0200

    Premier Commit + Intégration Firebas + 5 Star Rating component + UUID

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBJpBGCgUK6oLnoMnN4wZkZldBCpVpNeQ0",
+    authDomain: "myapp-3100b.firebaseapp.com",
+    databaseURL: "https://myapp-3100b.firebaseio.com",
+    projectId: "myapp-3100b",
+    storageBucket: "myapp-3100b.appspot.com",
+    messagingSenderId: "270880109120",
+    appId: "1:270880109120:web:1241bc10d32fcf55e925a5"
+  }
+ };
 
 /*
  * For easier debugging in development mode, you can import the following file
